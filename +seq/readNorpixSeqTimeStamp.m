a3741eff73347f36b69c0201e3f99e12a11357ee
Azim J <57653434+azimj@users.noreply.github.com>
2021-06-07 16:41:47 -0600

    Time Stamp: simplify time stamp extraction
    Each frame is a fixed size (header_data.TrueImageSize). The last 4
    bytes is a time stamp code.
    
    This change simplifies calculating the start of the timeStamp block
    for a specific frame.

--- +seq/readNorpixSeqTimeStamp.m
+++ +seq/readNorpixSeqTimeStamp.m
@@ -13,7 +13,6 @@ p.addRequired('seq_file_name',@(x)isfile(x));
 p.addOptional('StartFrame',1,@isnumeric);
 p.addOptional('EndFrame',NaN,@isnumeric);
 p.addParameter('SeqHeader',struct.empty,@isstruct);
-p.addParameter('endianType','ieee-le',@(x)strcmpi(x,'ieee-le')|strcmpi(x,'ieee-be'));
 p.parse(varargin{:});
 Results=p.Results;
 
@@ -21,21 +20,12 @@ Results=p.Results;
 if isnan(Results.EndFrame), Results.EndFrame=Results.StartFrame; end
 if isempty(Results.SeqHeader), Results.SeqHeader=seq.readNorpixSeqHeader(Results.seq_file_name); end
 
-switch Results.SeqHeader.imageBitDepthReal
-    case 8
-        bitsPerPixel=8;
-    case {12,14,16}
-        bitsPerPixel=16;
-    otherwise
-        errror('seq:readNorpixTimestamp:wrongBitDepth',['Unsupported bit depth: ' num2str(SeqHeader.imageBitDepthReal)]);
-end
-
-bitsPerByte=8;
-bytesPerPixel=bitsPerPixel/bitsPerByte;
-bytesInFrame = Results.SeqHeader.imageWidth*Results.SeqHeader.imageHeight*bytesPerPixel;
+headerSizeBytes = Results.SeqHeader.HeaderSize;
+endianType = Results.SeqHeader.endianType;
+imageSizeBytes = Results.SeqHeader.imageSizeBytes;
 %% open file and read data
 
-fid = fopen(Results.seq_file_name,'r',Results.endianType);
+fid = fopen(Results.seq_file_name,'r',endianType);
 clnFCN = onCleanup(@()fclose(fid));
 
 
@@ -44,12 +34,11 @@ tstr(nframes,1)="";
 tnum=zeros(nframes,1);
 for read_idx=1:nframes
     frameNumber = (read_idx-1)+Results.StartFrame; % -1 takes care of off-by-one-error
-    priorFrameBytes = (frameNumber-1)*Results.SeqHeader.TrueImageSize; % -1 takes care of off-by-one-error
+    framePos =  headerSizeBytes + ...
+                (frameNumber-1)*Results.SeqHeader.TrueImageSize; % -1 takes care of off-by-one-error
+    timeStampPos = framePos + imageSizeBytes;
     
-    fpos_bytes = Results.SeqHeader.HeaderSize ... %header size
-               + priorFrameBytes ... % number of bytes in previous frames
-               + bytesInFrame; % number of bytes in current frame
 
-    fseek(fid,fpos_bytes,'bof');
+    fseek(fid,timeStampPos,'bof');
     [tnum(read_idx), tstr(read_idx,1)]=seq.extractTimeStamp(fid);
 end
